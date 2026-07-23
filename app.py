bf1a6e87583f4b269a502828658b2a412f71d866
Guru4888 <singhgurmail488@gmail.com>
2026-07-23 23:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import eventlet
-eventlet.monkey_patch()
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, emit
 import os
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ghost_secret_key_123'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
 APP_PASSWORD = "1234"
 CHAT_PASSWORD = "abcd"
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -17,20 +18,25 @@ def login():
         else:
             return render_template('login.html', error="Password galat hai bhai")
     return render_template('login.html')
+
 @app.route('/chat')
 def chat():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('chat.html')
+
 @socketio.on('verify_password')
 def verify_password(data):
     if data['password'] == CHAT_PASSWORD:
         emit('password_status', {'status': 'ok'})
     else:
         emit('password_status', {'status': 'wrong'})
+
 @socketio.on('send_message')
 def handle_message(data):
-    emit('receive_message', data, broadcast=True)
+    if data:
+        emit('receive_message', data, broadcast=True)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
