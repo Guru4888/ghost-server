4231e5222054c977990a6692d9c34b965f8ddf8b
Guru4888 <singhgurmail488@gmail.com>
2026-07-24 00:11:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.secret_key = "ghostsecret12345"  # session ke liye zaruri
-socketio = SocketIO(app)
+app.secret_key = "ghostsecret12345"  # session ke liye zaruri hai
+socketio = SocketIO(app, async_mode='eventlet')
 
+# Yaha apne password change kar lena
 APP_PASSWORD = "1234"
 CHAT_PASSWORD = "abcd"
 
@@ -14,7 +15,7 @@ def login():
         password = request.form.get("password")
         if password == APP_PASSWORD:
             session["logged_in"] = True
-            return redirect("/chat")  # login ke baad chat pe bhej dega
+            return redirect(url_for('chat'))
         else:
             return render_template("login.html", error="Wrong Password!")
     return render_template("login.html")
@@ -22,7 +23,7 @@ def login():
 @app.route("/chat")
 def chat():
     if not session.get("logged_in"):
-        return redirect("/")
+        return redirect(url_for('login'))
     return render_template("chat.html")
 
 @socketio.on('verify_password')
@@ -32,9 +33,4 @@ def verify(data):
     else:
         emit('password_status', {'status': 'wrong'})
 
-@socketio.on('send_message')
-def handle_msg(data):
-    emit('receive_message', data, broadcast=True)
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+@socketio.on
