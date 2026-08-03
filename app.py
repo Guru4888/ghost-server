be8c3450253801db2f7dc86525aa17d370d0b46e
Guru4888 <singhgurmail488@gmail.com>
2026-08-03 22:18:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = "ghost_super_secret_key_998877"
 # Aapka Main App Password
 APP_PASSWORD = "guru&guru16230"
 
-# Bina eventlet ke standard SocketIO setup (Python 3.14 compatible)
+# Standard SocketIO setup (Render compatible)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 FAKE_404_HTML = """
